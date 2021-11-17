c018358644bc242f7d7b07faa63f14c37a4e6967
Amogh Prabhu <amoghprabhu@hotmail.com>
2021-11-17 12:51:40 +0530

    Added animation to card buttons

--- pothole/lib/widgets/GridWidgets/custom_grid_tile.dart
+++ pothole/lib/widgets/GridWidgets/custom_grid_tile.dart
@@ -2,7 +2,7 @@
 
 import 'package:flutter/material.dart';
 
-class CustomGridTile extends StatelessWidget {
+class CustomGridTile extends StatefulWidget {
   final String image;
   final String text;
   final String route;
@@ -15,27 +15,41 @@ class CustomGridTile extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<CustomGridTile> createState() => _CustomGridTileState();
+}
+
+class _CustomGridTileState extends State<CustomGridTile> {
+  double elv = 5;
+
+  @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () => Navigator.pushNamed(context, route),
+      onTap: () async {
+        elv = 1;
+        setState(() {});
+        await Future.delayed(Duration(milliseconds: 200));
+        elv = 5;
+        setState(() {});
+        Navigator.pushNamed(context, widget.route);
+      },
       child: Card(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
         ),
-        elevation: 5,
+        elevation: elv,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             Expanded(
               child: Padding(
                 padding: const EdgeInsets.only(top: 22),
-                child: Image.asset(image),
+                child: Image.asset(widget.image),
               ),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text(
-                text,
+                widget.text,
                 textScaleFactor: 1.5,
                 style: TextStyle(fontWeight: FontWeight.bold),
               ),
