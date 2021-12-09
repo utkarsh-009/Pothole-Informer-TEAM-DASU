b97a3a13121271c0af1c8f1d47e3c3bbec199039
Soumitra <soumitrakand3@gmail.com>
2021-12-09 12:29:57 +0530

    Temp Admin page

--- pothole/lib/pages/admin_homepage.dart
+++ pothole/lib/pages/admin_homepage.dart
@@ -1,60 +1,170 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
-import 'dart:io';
+// ignore_for_file: prefer_const_constructors
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import '../utils/themes.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pothole/utils/themes.dart';
 
 class AdminHomePage extends StatefulWidget {
+  const AdminHomePage({Key? key}) : super(key: key);
+
   @override
-  State<AdminHomePage> createState() => _AdminHomePageState();
+  _AdminHomePageState createState() => _AdminHomePageState();
 }
 
 class _AdminHomePageState extends State<AdminHomePage> {
-  List<String> imagePaths = [];
+  final CollectionReference potholeList =
+      FirebaseFirestore.instance.collection("Pothole Details");
+
+  List potholeItems = [];
+  Future fetchPotholeList() async {
+    await potholeList.get().then((querySnapshot) {
+      for (var element in querySnapshot.docs) {
+        potholeItems.add(element.data());
+      }
+    });
+    setState(() {});
+  }
 
   @override
   void initState() {
     super.initState();
-    FirebaseFirestore.instance
-        .collection("Pothole Details")
-        .snapshots()
-        .listen((event) {
-      setState(() {
-        imagePaths = List.generate(event.docs.length,
-            (index) => event.docs[index].data()['image url']);
-      });
-    });
-  }
-
-  Future<String> downloadURL(String imageURL) async {
-    String downloadUrl =
-        await FirebaseStorage.instance.ref(imageURL).getDownloadURL();
-    return downloadUrl;
+    fetchPotholeList();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: MyThemes.creamColor,
-      appBar: AppBar(
-        title: Text("Admin Homepage hello"),
-      ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(32.0),
-            child: Card(
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 18,
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(50),
               child: Container(
-                height: 500,
-                child: Image.network(imagePaths[index]),
+                width: double.infinity,
+                padding:
+                    const EdgeInsets.symmetric(vertical: 18, horizontal: 30),
+                child: Text(
+                  "Admin Page",
+                  style: TextStyle(
+                      color: MyThemes.darkBluishColor,
+                      fontFamily: GoogleFonts.lato().fontFamily,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold),
+                ),
               ),
             ),
+            ItemHolder(potholeItems: potholeItems)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ItemHolder extends StatelessWidget {
+  const ItemHolder({Key? key, required this.potholeItems}) : super(key: key);
+  final dynamic potholeItems;
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemBuilder: (context, index) {
+          return PotholeWidget(
+            item: potholeItems[index],
           );
         },
-        itemCount: imagePaths.length,
+        itemCount: potholeItems.length,
+      ),
+    );
+  }
+}
+
+class PotholeWidget extends StatelessWidget {
+  final dynamic item;
+
+  const PotholeWidget({Key? key, required this.item}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 10),
+      child: Card(
+        elevation: 8,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 350,
+              child: Stack(
+                children: [
+                  Positioned.fill(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(18),
+                      child: Image.network(
+                        item["image url"],
+                        fit: BoxFit.cover,
+                        loadingBuilder: (context, child, loadingProgress) {
+                          if (loadingProgress == null) return child;
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        },
+                        errorBuilder: (context, error, stackTrace) =>
+                            const Text('Some errors occurred!'),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    child: Container(
+                      height: 310,
+                      decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.only(
+                          bottomLeft: Radius.circular(18),
+                          bottomRight: Radius.circular(18),
+                        ),
+                        gradient: LinearGradient(
+                            colors: [
+                              Colors.black.withOpacity(0.5),
+                              Colors.transparent,
+                            ],
+                            begin: Alignment.bottomLeft,
+                            end: Alignment.centerLeft),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    child: SizedBox(
+                      width: 350,
+                      child: Padding(
+                        padding: const EdgeInsets.all(18),
+                        child: Text(
+                          item['Description of Pothole'],
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontFamily: 'Raleway'),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(18),
+        ),
       ),
     );
   }
